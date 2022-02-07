4ba97cc076e99192213375f60dab8841eb8eb2fd
linneyr8 <rl677@cam.ac.uk>
2022-02-07 16:22:02 +0000

    Fake stations

--- test_geo.py
+++ test_geo.py
@@ -1,13 +1,34 @@
 from floodsystem.geo import stations_by_distance
 from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
-import pytest
+from floodsystem.station import MonitoringStation
 
+radius = float(10)
+p = (52.2053, 0.1218)
 
-stations = build_station_list()
-
-def test_stations_within_radius():
-    assert len(stations_within_radius(stations, (52.2053, 0.1218), 0)) == 0
-    assert len(stations_within_radius(stations, (52.2053, 0.1218), 10)) > 0
-    assert len(stations_within_radius(stations, (52.2053, 0.1218), 1000)) > len(stations_within_radius(stations, (52.2053, 0.1218), 10))
-
+def test_stations_by_distance():
+    fake_station1 = MonitoringStation(
+        station_id = "fs1",
+        measure_id = "gg",
+        label = "fs1",
+        coord = (float(50), float(50)),
+        typical_range = (float(10), float(20)),
+        river = "r1",
+        town = "t1")
+    fake_station2 = MonitoringStation(
+        station_id = "fs2",
+        measure_id = "gg",
+        label = "fs2",
+        coord = (float(60), float(60)),
+        typical_range = (float(52.21), float(0.123)),
+        river = "r2",
+        town = "t2")
+    fake_station3 = MonitoringStation(
+        station_id = "fs3",
+        measure_id = "gg",
+        label = "fs3",
+        coord = (float(70), float(70)),
+        typical_range = (float(1), float(19)),
+        river = "r3",
+        town = "t3")
+    test_stations = [fake_station1, fake_station2, fake_station3]
