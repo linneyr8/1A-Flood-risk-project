1eacbca4db42d3f1fc34b62dfb3e57bfa25a41b2
Nicole Rix <67520066+nicolerix@users.noreply.github.com>
2022-02-08 12:55:18 +0000

    Updated to sort 1F

--- Task1F.py
+++ Task1F.py
@@ -6,7 +6,7 @@ from floodsystem.station import inconsistent_typical_range_stations
 
 def run():
     stations = build_station_list() 
-    inconsistent_stations= inconsistent_typical_range_stations(stations)
+    inconsistent_stations= sorted(inconsistent_typical_range_stations(stations))
 
     print(inconsistent_stations)
     
