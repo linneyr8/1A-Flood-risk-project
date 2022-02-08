8ba5a535d65d49cddf90601902127fa8d7ba2b25
Nicole Rix <67520066+nicolerix@users.noreply.github.com>
2022-02-08 09:50:49 +0000

    Fixing the problems of Task1D

--- Task1D.py
+++ Task1D.py
@@ -4,17 +4,28 @@ from floodsystem.geo import stations_by_river
 from floodsystem.stationdata import build_station_list
 
 def run():
+        """"
+        Print how many rivers have at least one monitoring station
+        prints the first 10 of these rivers in alphabetical order
+        print the names of the stations located on the following rivers in alhabetical order
+           'River Aire'
+           'River Cam'
+           'River Thames'
+         """
         stations= build_station_list()
+        rivers = rivers_with_station(stations)
 
-        output= rivers_with_station(stations)
-        print(len(output))
-        print(output[0:10])
+        print(len(rivers))
+        print(sorted(rivers)[:10])
+        
+        print()
 
-        river_dictionary = stations_by_river(stations)
-
-        print(river_dictionary["River Aire"])
-        print(river_dictionary["River Cam"])
-        print(river_dictionary["River Thames"])
+        stations_on_river = stations_by_river(stations)
+        for river in ['River Aire', 'River Cam', 'River Thames']:
+                print(river + ':')
+                print(sorted([i.name for i in stations_on_river[river]]))
+                
+                                
 if __name__ == "__main__":
     print("*** Task 1D: CUED Part 1A Flood Warning System ***") 
     run()  
