3920a7acebb2fe13b05f9fbac6c16e1aa5a0e810
linneyr8 <rl677@cam.ac.uk>
2022-02-10 19:27:25 +0000

    Fixed

--- Task1D.py
+++ Task1D.py
@@ -3,27 +3,17 @@ from floodsystem.geo import rivers_with_station
 from floodsystem.geo import stations_by_river
 from floodsystem.stationdata import build_station_list
 
-def run():
-        """"
-        Print how many rivers have at least one monitoring station
-        prints the first 10 of these rivers in alphabetical order
-        print the names of the stations located on the following rivers in alhabetical order
-           'River Aire'
-           'River Cam'
-           'River Thames'
-         """
-        stations= build_station_list()
-        rivers = rivers_with_station(stations)
+stations = build_station_list()
+print (stations[0])
 
-        print(len(rivers))
-        print(sorted(rivers)[:10])
+output = rivers_with_station(stations)
+print(len(output))
+#print(output[0:10])
 
-        stations_on_river = stations_by_river(stations)
-        for river in ['River Aire', 'River Cam', 'River Thames']:
-                print(rivers , ':')
-                print(sorted([i.name for i in stations_on_river[rivers]]))
-                
+river_dictionary = stations_by_river(stations)
+print("River Aire:", river_dictionary["River Aire"])
+print("River Cam:", river_dictionary["River Cam"])
+print("River Thames:", river_dictionary["River Thames"])
                                 
 if __name__ == "__main__":
-    print("*** Task 1D: CUED Part 1A Flood Warning System ***") 
-    run()  
+    print("*** Task 1D: CUED Part 1A Flood Warning System ***")
