e21337d853a07e2c4d979383a7c94044b2e3c7b9
linneyr8 <rl677@cam.ac.uk>
2022-02-15 11:20:43 +0000

    alteer

--- Task1E.py
+++ Task1E.py
@@ -5,11 +5,12 @@
 from floodsystem.geo import rivers_by_station_number
 from floodsystem.stationdata import build_station_list 
 
-stations = build_station_list()
-N = 9
-station_number = rivers_by_station_number(stations, N)
-
-print(station_number)
 
 if __name__ == "__main__":
     print("*** Task 1E: CUED Part 1A Flood Warning System ***")
+
+    stations = build_station_list()
+    N = 9
+    station_number = rivers_by_station_number(stations, N)
+
+    print(station_number)
