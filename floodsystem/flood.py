b06761180f5c90a5d088df1656c8687095bf60f6
linneyr8 <rl677@cam.ac.uk>
2022-02-17 12:04:19 +0000

    new function

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -8,3 +8,11 @@ def stations_level_over_threshold(stations, tol):
         [(station, station.relative_water_level()) for station in stations if station.relative_water_level() is not None]
     ), 1, reverse=True)
 
+def stations_highest_rel_level(stations, N):
+    return sorted(filter(
+        lambda x: x.relative_water_level() is not None,
+        stations
+    ),
+        key=lambda x: x.relative_water_level(),
+        reverse=True
+    )[:N]
