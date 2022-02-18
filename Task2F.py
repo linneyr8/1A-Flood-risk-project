c0564c23ce503f05cade28033bc8238a3df904f7
Nicole Rix <67520066+nicolerix@users.noreply.github.com>
2022-02-18 16:57:24 +0000

    Importing the correct functions

--- Task2F.py
+++ Task2F.py
@@ -1,1 +1,12 @@
+from datetime import timedelta
 
+from bokeh.io import show
+
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.plot import plot_water_level_with_fit
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+def run():
+  stations= build_station_list()
+  
