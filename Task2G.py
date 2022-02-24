7793098053bb3bb3d95d60872d0fd7d7afd5ba08
Nicole Rix <67520066+nicolerix@users.noreply.github.com>
2022-02-24 01:53:59 +0000

    Creating/stating code 2G

---
+++ Task2G.py
@@ -0,0 +1,15 @@
+from floodsystem.food import staions_highest_rel_level
+
+
+""" Issueing flood warnings for towns
+args: function that runs through each station's water level data ad comparing it to standard water levels which classify it to a certain class
+return: list of towns with severity of risk 
+
+severe when levels>
+high when <levels<
+moderate when <levels<
+low when <levels """
+
+
+
+
