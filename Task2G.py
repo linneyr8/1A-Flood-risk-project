b87a6517cd874263b75eafa5a9b58bc37d9014c8
Nicole Rix <67520066+nicolerix@users.noreply.github.com>
2022-03-01 11:37:21 +0000

    Update to work

--- Task2G.py
+++ Task2G.py
@@ -1,15 +1,44 @@
-from floodsystem.flood import staions_highest_rel_level
-from floodsystem.flood import fetch_measure_levels 
+"""Flood Warning System
 
-""" Issueing flood warnings for towns
-args: function that runs through each station's water level data ad comparing it to standard water levels which classify it to a certain class
-return: list of towns with severity of risk 
+returns either, "high" "moderate" or "low" risk of flooding
 
-severe when levels>
-high when <levels<
-moderate when <levels<
-low when <levels """
+probability of flooding is computed via a range of inputs,
+
+current level over the typical maximum, the higher the current
+level the higher the assumed risk of flooding (if in flood flood level high)
+relative level, if the level is relatively high a similar equation is used
+"""
+from floodsystem.stationdata import build_station_list, update_water_levels
+import floodsystem.flood as flood
 
 
+def run():
+    """Task 2G"""
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    output = flood.assess_risk(stations)
+    print("Risk of all stations at the moment")
+    print(output)
+
+    print("\nStations Currently at High Risk of Flood\n")
+    for data in output:
+        if data[1] == 'High':
+            print(data)
+
+
+    """
+    flood_data = flood.assess_risk(stations)
+    for data in flood_data:
+        print(data[0], data[1], data [2])"""
+
+
+
+if __name__ == "__main__":
+    print("*** Task 2G: CUED Part IA Flood Warning System ***")
+    run()
 
 
