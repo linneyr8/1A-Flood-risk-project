e16b49963605efc8821bb303a092fc1693246195
Nicole Rix <67520066+nicolerix@users.noreply.github.com>
2022-03-01 14:03:44 +0000

    Putting in function for flood risk

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -78,3 +78,69 @@ def stations_level_over_threshold(stations, tol):
         [(station, station.relative_water_level()) for station in stations if
          station.relative_water_level() is not None]
     ), 1, reverse=True)
+
+
+""" For task 2G the function that assess the flood risks """
+import numpy as np
+
+def assess_risk(stations):
+    """assess_risk(stations), returns a (station.name, station risk) in order of risk """
+
+    dtype = [('stationName', str), ('value', float)]
+    #computing current level risk
+    current_levels = np.array(stations_highest_rel_level_wrong(stations, len(stations)+1))
+    
+    #1m above max has a risk of 1, 0.4 m below max has risk of 0
+    levelRisk = current_levels[:, 1].astype(float)
+    levelRisk = (1)/(1.4)*(levelRisk + 0.4)
+    levelRisk = np.array([[current_levels[i, 0].astype(str), levelRisk[i]] for i in range(len(levelRisk))])
+    
+
+    #relative level risk
+    #relative level 1.5 risk 1, relative level 0 risk 0
+    rellevels = np.array([[station.name, station.relative_water_level()/1.5] if station.relative_water_level() != None else [station.name, station.relative_water_level()] for station in stations])
+    
+    levelRisk = levelRisk[np.argsort(levelRisk[:,0])]
+    rellevels = rellevels[np.argsort(rellevels[:,0])]
+
+    #levelRisk = sorted(levelRisk, key=lambda x: x[0])
+    #rellevels = sorted(rellevels, key=lambda x: x[0])
+    #print(levelRisk.shape)
+    #print(rellevels.shape)
+    #print(len(stations))
+    StationsAndRisk = np.empty((len(levelRisk),2), dtype=object)
+
+    skipstationcounter = 0
+
+    for i in range(len(levelRisk)):
+        StationsAndRisk[i, 0] = levelRisk[i, 0]
+        if (levelRisk[i,0] == rellevels[i+skipstationcounter,0]):
+            if rellevels[i+skipstationcounter,1] != None:
+                #print(levelRisk[i,1], rellevels[i,1])
+                StationsAndRisk[i,1] = ((float(levelRisk[i,1]) + float(rellevels[i+skipstationcounter,1]))/2)
+            else:
+                StationsAndRisk[i,1] = (levelRisk[i,1])
+        else:
+            #print("ERROR!!! STATIONS dont match")
+            #print(levelRisk[i,0], rellevels[i+skipstationcounter,0])
+            #print("in position", i)
+
+            StationsAndRisk[i,1] = levelRisk[i,1]
+            skipstationcounter += 1
+
+            while(levelRisk[i,0] != rellevels[i+skipstationcounter,0]):
+                #print(levelRisk[i+skipstationcounter,0], rellevels[i,0])
+                skipstationcounter += 1
+            
+    #output array generation
+    output = np.empty((len(StationsAndRisk), 2), dtype=object)
+    for i, value in enumerate(StationsAndRisk):
+        output[i,0] = value[0]
+        if float(value[1]) > 0.7:
+            output[i,1] = "High"
+        elif float(value[1]) > 0.4:
+            output[i,1] = "Moderate"
+        else:
+            output[i,1] = "Low"
+    
+    return(output)
