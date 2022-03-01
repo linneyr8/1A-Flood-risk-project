ecb9db661f4be95612e9ddd51208dc0b157d4db8
Nicole Rix <67520066+nicolerix@users.noreply.github.com>
2022-03-01 12:20:38 +0000

    reverted back

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,72 +1,38 @@
-"""Flood Submodule"""
-#from curses.ascii import NUL
-import string
-from turtle import st
-from unittest import skip
-import numpy as np
+from .utils import sorted_by_key
+from floodsystem.station import MonitoringStation
 
-from floodsystem.geo import stations_by_river
-from .utils import sorted_by_key  # noqa
+def stations_level_over_threshold(stations, tol):
+    """
+    Function that returns stations whose latest relative water level is over some threshold.
+    Args:
+        stations (list): List of stations (MonitoringStation).
+        tol (float): The threshold relative water level.
+    Returns:
+        list: List of tuples in the format (station (MonitoringStation),
+        relative water level) sorted by the relative level in descending order.
+    """
 
-from .station import MonitoringStation
-
-
-from floodsystem.stationdata import build_station_list, update_water_levels
+    return sorted_by_key(filter(
+        lambda x: x[1] > tol,
+        [(station, station.relative_water_level()) for station in stations if
+         station.relative_water_level() is not None]
+    ), 1, reverse=True)
 
 
 def stations_highest_rel_level(stations, N):
-    """stations_highest_rel_level(stations, N) returns a list of N stations in which the water level is closest to the maximum"""
-    rel_levels = stations_level_over_threshold(stations, -50)
+    """
+    Function that returns the N number of most at risk stations.
+    Args:
+        stations (list): List of stations (MonitoringStation).
+        N (int): Length of the desired list
+    Returns:
+        list: List of stations (MonitoringStation).
+    """
+    return sorted(filter(
+        lambda x: x.relative_water_level() is not None,
+        stations
+    ),
+        key=lambda x: x.relative_water_level(),
+        reverse=True
+    )[:N]
 
-    return rel_levels[:N]
-
-
-def stations_highest_rel_level_wrong(stations, N):
-    """stations_highest_rel_level_wrong(stations, N) returns a list of N stations in which the water level is closest to the maximum"""
-    difference = []
-    if stations[0].latest_level == None:
-        update_water_levels(stations)
-
-    for station in stations:
-        if station.typical_range_consistent():
-            try:
-                if (station.latest_level - station.typical_range[1]) < 50:
-                    difference.append((station.name, (station.latest_level - station.typical_range[1])))
-
-            except Exception:
-                difference.append((station.name, -5000))
-                # if station.latest_level == None
-        else:
-            difference.append((station.name, -5000))
-
-    difference = sorted(difference, key=lambda x: -x[1])
-    #print(difference)
-    return difference[:N]
-
-
-
-def stations_level_over_threshold(stations, tol):
-    """returns a list of tuples containing (station, relative water level at the station) 
-    for which the relative water level is over tol. tuples are sorted by relative level in descending order"""
-
-    #update_water_levels(stations)
-
-    #set up empty list
-    stations_over_tol = []
-
-    #iterate through all stations and check if relative level is over tol
-    for station in stations:
-        #print('tring if')
-        if station.typical_range_consistent() and (station.relative_water_level() != None):
-            #print("consistent", station)
-            if (station.relative_water_level() > tol):
-                #print('adding station' , station)
-                stations_over_tol.append((station, station.relative_water_level()))
-        else:
-            pass
-    
-    #sort by relative level
-    stations_over_tol = sorted(stations_over_tol, key = lambda x:-x[1])
-    
-
-    return 
