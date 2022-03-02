7dcf2f3da8379f20a7437a7a98bd8074fa38b7c5
linneyr8 <rl677@cam.ac.uk>
2022-03-02 10:24:15 +0000

    making 2G simpler

--- Task2G.py
+++ Task2G.py
@@ -18,14 +18,22 @@ def plot_risk(stations):
             High += 1
         elif station.risk == 'Severe':
             Severe += 1
+    
+    #creating array of the risks to later implement in the x axis  
     x_pos = np.arange(4)
     Risk = ('Low', 'Moderate', 'High', 'Severe')
     Values = (Low, Moderate, High, Severe)
-    plt.bar(x_pos, Values, align='center')
-    plt.xticks(x_pos, Risk, rotation=45)
 
+    #setting up the x cordinates then y cordinates of the bar graph
+    plt.bar(x_pos, Values, align='center')
+
+    #setting up the current tick locations and labels of the x-axis 
+    plt.xticks(x_pos, Risk, rotation=45)
+    
+    #labelling the x and y axis -- X axis= is level of risk -- Y axis is the number of stations 
     plt.xlabel('Risk')
     plt.ylabel('Number of Stations')
 
+    #printing just the values/points which would of been plotted on the graph 
     plt.show()
 
