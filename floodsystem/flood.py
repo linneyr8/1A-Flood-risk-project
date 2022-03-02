bcba938db4a0faa972ef4c754ecc9396ac80b0ac
Nicole Rix <67520066+nicolerix@users.noreply.github.com>
2022-03-02 00:33:28 +0000

    flood

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -40,24 +40,33 @@ def stations_highest_rel_level(stations, N):
 #Nicole Code
 def stations_highest_rel_level_wrong(stations, N):
     """stations_highest_rel_level_wrong(stations, N) returns a list of N stations in which the water level is closest to the maximum"""
+    
+    #creating an empty list defined as difference
     difference = []
+    
+    #If the stations latest water level data compared is null then run the function which attach level data contained in measure_data to stations
     if stations[0].latest_level == None:
         update_water_levels(stations)
 
+    """if typical_range_consistent returns true if the data is consistent then find the difference between the stations latest water 
+    level and its typical water level range. If the difference is less than 50 add to the empty list defined as difference which was 
+    created at the start of the function"""
     for station in stations:
         if station.typical_range_consistent():
             try:
                 if (station.latest_level - station.typical_range[1]) < 50:
                     difference.append((station.name, (station.latest_level - station.typical_range[1])))
-
+                    
+    """"if station.latest_level == None"""
             except Exception:
                 difference.append((station.name, -5000))
-                # if station.latest_level == None
+                
+    """Sort the list created--- by taking arg x and returning -x[1]"""
         else:
-            difference.append((station.name, -5000))
-
+            difference.append((station.name, -5000  
     difference = sorted(difference, key=lambda x: -x[1])
-    #print(difference)
+                               
+    """print(difference"""
     return difference[:N]
 
 
@@ -85,21 +94,26 @@ import numpy as np
 
 def assess_risk(stations):
     """assess_risk(stations), returns a (station.name, station risk) in order of risk """
-#dtype 
+    
+    """describing an array of items--- in this case assigning station name as a string and the value as a float"""
     dtype = [('stationName', str), ('value', float)]
-    #computing current level risk= creating a array of data of stations of N stations in which the water level is closest to the maximum. Iterating over the list of stations.  
+                               
+    """computing current level risk= creating a array of data of stations of N stations in which the water level 
+    is closest to the maximum. Iterating over the list of stations"""  
     current_levels = np.array(stations_highest_rel_level_wrong(stations, len(stations)+1))
     
-    #1m of the water above max level has a risk of 1, 0.4 m below max has risk of 0. Trying to get rid of the offset so below o.4m of the max= 0
+    """1m of the water above max level has a risk of 1, 0.4 m below max has risk of 0. Trying to get rid of the offset so below o.4m of the max water level= 0"""
     levelRisk = current_levels[:, 1].astype(float)
     levelRisk = (1)/(1.4)*(levelRisk + 0.4)
-    #setting the risk level into an numpy array 
+                               
+    """setting the risk level into an numpy array= containing data of the current water levels. 
+    astype(str) is used to change the data type of the current water levels into a string. Iterates through each of the stations"""
     levelRisk = np.array([[current_levels[i, 0].astype(str), levelRisk[i]] for i in range(len(levelRisk))])
     
-
+    
     #relative level risk
     #relative level 1.5 risk 1, relative level 0 risk 0
-    rellevels = np.array([[station.name, station.relative_water_level()/1.4] if station.relative_water_level() != None else [station.name, station.relative_water_level()] for station in stations])
+    rellevels = np.array([[station.name, station.relative_water_level()/1.5] if station.relative_water_level() != None else [station.name, station.relative_water_level()] for station in stations])
     
     levelRisk = levelRisk[np.argsort(levelRisk[:,0])]
     rellevels = rellevels[np.argsort(rellevels[:,0])]
@@ -133,7 +147,7 @@ def assess_risk(stations):
                 #print(levelRisk[i+skipstationcounter,0], rellevels[i,0])
                 skipstationcounter += 1
             
-    #output array generation
+    """returning a new array of given shape, dtype """
     output = np.empty((len(StationsAndRisk), 2), dtype=object)
     for i, value in enumerate(StationsAndRisk):
         output[i,0] = value[0]
