0c104da6e0ba7c1f365466bcd1b58f8e6f3e0a80
linneyr8 <rl677@cam.ac.uk>
2022-03-02 10:09:29 +0000

    making 2G more simple

--- Task2G.py
+++ Task2G.py
@@ -1,44 +1,31 @@
-"""Flood Warning System
-
-returns either, "high" "moderate" or "low" risk of flooding
-
-probability of flooding is computed via a range of inputs,
-
-current level over the typical maximum, the higher the current
-level the higher the assumed risk of flooding (if in flood flood level high)
-relative level, if the level is relatively high a similar equation is used
-"""
-from floodsystem.stationdata import build_station_list, update_water_levels
-import floodsystem.flood as flood
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+from floodsystem.analysis import polyfit
 
 
-def run():
-    """Task 2G"""
-    # Build list of stations
-    stations = build_station_list()
+def plot_risk(stations):
+    Low = 0
+    Moderate = 0
+    High = 0 
+    Severe = 0
+    for station in stations:
+        if station.risk == 'Low':
+            Low += 1
+        elif station.risk == 'Moderate':
+            Moderate += 1
+        elif station.risk == 'High':
+            High += 1
+        elif station.risk == 'Severe':
+            Severe += 1
+    x_pos = np.arange(4)
+    Risk = ('Low', 'Moderate', 'High', 'Severe')
+    Values = (Low, Moderate, High, Severe)
+    plt.bar(x_pos, Values, align='center')
+    plt.xticks(x_pos, Risk, rotation=45)
 
-    # Update latest level data for all stations
-    update_water_levels(stations)
+    plt.xlabel('Risk')
+    plt.ylabel('Number of Stations')
 
-    output = flood.assess_risk(stations)
-    print("Risk of all stations at the moment")
-    print(output)
-
-    print("\nStations Currently at High Risk of Flood\n")
-    for data in output:
-        if data[1] == 'High':
-            print(data)
-
-
-    """
-    flood_data = flood.assess_risk(stations)
-    for data in flood_data:
-        print(data[0], data[1], data [2])"""
-
-
-
-if __name__ == "__main__":
-    print("*** Task 2G: CUED Part IA Flood Warning System ***")
-    run()
-
+    plt.show()
 
